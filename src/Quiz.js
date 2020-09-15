b77ee9bddae809c76f7434c81f405dc0e61192fe
mrgavino <soygavino@gmail.com>
2020-09-15 10:02:49 -0600

    Fix Quiz component

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,13 +2,10 @@ import React, { Component } from "react";
 
 let quizData = require('./quiz_data.json');
 
-export class Quiz extends Component {
+class Quiz extends Component {
     constructor(props) {
-        super(props);
-
-        this.state = {
-            quiz_position: 1
-        }
+        super(props)
+        this.state = { quiz_position: 1 }
     }
 
     render() {
@@ -16,7 +13,7 @@ export class Quiz extends Component {
             <div>
                 <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
             </div>
-        );
+        )
     }
 }
 export default Quiz
