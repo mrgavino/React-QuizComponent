5f82167d55dbdfab353c95e772b3481b9f8f5a30
mrgavino <soygavino@gmail.com>
2020-09-15 16:21:02 -0600

    Add solution to module 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,6 +15,10 @@ class Quiz extends Component {
             quiz_position: this.state.quiz_position + 1
         })
     }
+
+    handleResetClick() {
+        this.setState({quiz_position: 1});
+    }
     
 
     render() {
@@ -22,7 +26,7 @@ class Quiz extends Component {
         return (
             <div>
                 {isQuizEnd
-                    ? <QuizEnd />
+                    ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>
                     : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />
                 }
             </div>
